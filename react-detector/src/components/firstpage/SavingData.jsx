6c991d37de630d45f365e350dd38e63846a1be0b
Joshua Teoxon <teoxonjoshua13@gmail.com>
2023-12-26 10:53:37 +0800

    commit all

--- react-detector/src/components/firstpage/SavingData.jsx
+++ react-detector/src/components/firstpage/SavingData.jsx
@@ -3,7 +3,13 @@ const SavingData = (obj) => {
     method: "Post",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(obj),
-  });
+  })
+    .then((a) => {
+      if (!a.ok) {
+        throw new Error("it has the same id, it will not save");
+      }
+    })
+    .catch((er) => console.log(er));
 };
 
 export default SavingData;
